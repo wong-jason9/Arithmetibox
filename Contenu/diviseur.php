37848edcf0e7b6dae125d2b3ad18073aba778388
Linkoun <Linkoun93120@gmail.com>
2016-10-10 12:38:20 +0200

    Correction diviseur+changement nom bouton

--- Contenu/diviseur.php
+++ Contenu/diviseur.php
@@ -9,25 +9,29 @@
 <form action="Arithmetibox.php?outil=divi" method="POST">
 
 Nombre : <input size='30' name='nombre' type='text'><br>
-<input type='submit' value='Déchiffrer'  class="boutton">
+<input type='submit' value='Calculer'  class="boutton">
 
 </form>
 
 <?php
     if(isset($_POST['nombre'])){
-        echo "Les diviseurs de ".$_POST['nombre']." sont : ";
-        for($i=1; $i<$_POST['nombre'];$i++){
-            if($_POST['nombre']%$i==0){
-                echo $i." | ";
+        if(preg_match('#^[0-9]*$#',$_POST['nombre']) and $_POST['nombre']!=NULL){
+            echo "Les diviseurs de ".$_POST['nombre']." sont : ";
+            for($i=1; $i<=$_POST['nombre'];$i++){
+                if($_POST['nombre']%$i==0){
+                    echo $i." | ";
+                }
+            }
+            echo "</br>";
+            for($i=1; $i<=sqrt($_POST['nombre']);$i++){
+                if($_POST['nombre']%$i==0){
+                    echo $i." x ".($_POST['nombre']/$i);
+                    echo "</br>";
+                }
             }
         }
-        echo "</br>";
-        for($i=1; $i<=sqrt($_POST['nombre']);$i++){
-            if($_POST['nombre']%$i==0){
-                echo $i." x ".($_POST['nombre']/$i);
-                echo "</br>";
-            }
-        }
+        else
+            echo "Saisie incorrecte.";
     }
     ?>
 </body>
