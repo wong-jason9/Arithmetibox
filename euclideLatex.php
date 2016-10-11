07d0e8c0e40d83af0c2c8f2512123c0fa9f24068
sheww <jeremydossantos96@yahoo.fr>
2016-10-11 22:17:48 +0200

    Add files via upload
    Resolution euclide LaTeX a update

---
+++ euclideLatex.php
@@ -0,0 +1,37 @@
+<script type="text/javascript" async
+  src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML">
+</script>
+
+<form action="euclide.php">
+	<p>
+	Un nombre a :<input type="text" name="nbA" value=""/></br>
+	Un nombre b :<input type="text" name="nbB" value=""/></br>
+	<input type="submit" value="Calculer"></p>
+</form>
+
+
+<?php
+
+    function euclide($a, $b) {
+        while ($b!=0){
+            $t=$a%$b;
+            echo $a.' | '.$b.' | '.$t.' | '.(int)($a/$b).'</br>';
+            $a=$b;
+            $b=$t;
+        }
+        return $a;
+    }
+
+
+euclide($_GET['nbA'],$_GET['nbB']);
+
+?>
+
+
+$$
+\begin{array}{l | c | c |r}
+
+\end{array}
+$$
+
+
