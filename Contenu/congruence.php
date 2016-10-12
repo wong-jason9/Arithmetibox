d078dcc031733bc0ead78c096683ce4452945924
Linkoun <Linkoun93120@gmail.com>
2016-10-12 11:29:55 +0200

    add Latex dans congruence

--- Contenu/congruence.php
+++ Contenu/congruence.php
@@ -1,24 +1,46 @@
 <?php require('debut.php'); ?>
-<body>
-  <form action='Arithmetibox.php?outil=cong' method='post'>
-    <p>
-  		<input type="text" name='entierA'></input>
-  		modulo
-  		<input type="text" name='modulo'></input>
-  		est congru à ...
-  		<input type="submit" value="Calculer" class="boutton"/>
-  	</p>
-  </form>
-  <?php
-  	if( isset($_POST['entierA']) and isset($_POST['modulo'])
-  		and trim($_POST['entierA'])!='' and trim($_POST['modulo'])!=''
-  		and preg_match('#^-?[0-9]+$#', $_POST['entierA']) and preg_match('#^-?[0-9]+$#', $_POST['modulo'])
-  		) {
-  		echo $_POST['entierA'].' modulo '.$_POST['modulo'].' = ';
-  		echo $_POST['entierA']%$_POST['modulo'];
-  	}
-  	else
+<form action="Arithmetibox.php?outil=cong" method="post">
+<p>
+<input type="text" name='entierA'></input>
+modulo
+<input type="text" name='modulo'></input>
+est congru à ...
+<input type="submit"/>
+</p>
+</form>
+<?php
+    function partieEntiere($d){
+        if(preg_match('#\.#', $d)){
+            $tab = explode('.', $d);
+            return $tab[0];
+        }
+        else
+            return $d;
+    }
+    
+    if( isset($_POST['entierA']) and isset($_POST['modulo'])
+       and trim($_POST['entierA'])!='' and trim($_POST['modulo'])!=''
+       and preg_match('#^-?[0-9]+$#', $_POST['entierA']) and preg_match('#^-?[0-9]+$#', $_POST['modulo'])
+       ) {
+        //  \[\equiv \mod{}\]
+        echo '\['.$_POST['entierA'].'\equiv'."_{$_POST['modulo']}".$_POST['entierA']%$_POST['modulo'].'\]';
+        
+    }
+    else
   		echo '<p>Vérifiez vos saisies</p>';
-  ?>
- </body>
+    /*  \[\frac{numerateur}{denominateur}\] */
+    
+    //si division a/mod contient un '.', partie entiere
+    if(preg_match('#\.#', $_POST['entierA']/$_POST['modulo'])){
+        echo '\[ \lfloor \dfrac{'.$_POST['entierA'].'}{'.$_POST['modulo'].'} \rfloor = '.partieEntiere($_GET['entierA']/$_POST['modulo']).'\]';
+    }
+    else{
+        echo '\[\frac{'.$_POST['entierA'].'}{'.$_POST['modulo'].'} = '.$_POST['entierA']/$_POST['modulo'].'\]';
+    }
+    
+    echo '\['.$_POST['entierA'].' = '.partieEntiere($_POST['entierA']/$_POST['modulo']).' \times '.$_POST['modulo'].' + \boxed{'.$_POST['entierA']%$_POST['modulo'].'} \]';
+    
+    
+    ?>
+</body>
 </html>
