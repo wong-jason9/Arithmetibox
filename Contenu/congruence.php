f29deaab6fda9d3997d2766edd6332f39c5af9d3
jrKaing <jrkaing@MacBook-Air-de-JRK.local>
2016-10-14 11:14:20 +0200

    Modification Congruence

--- Contenu/congruence.php
+++ Contenu/congruence.php
@@ -19,27 +19,21 @@ est congru à ...
     }
     
     if( isset($_POST['entierA']) and isset($_POST['modulo'])
-       and trim($_POST['entierA'])!='' and trim($_POST['modulo'])!=''
-       and preg_match('#^-?[0-9]+$#', $_POST['entierA']) and preg_match('#^-?[0-9]+$#', $_POST['modulo'])
-       ) {
-        //  \[\equiv \mod{}\]
-        echo '\['.$_POST['entierA'].'\equiv'."_{$_POST['modulo']}".$_POST['entierA']%$_POST['modulo'].'\]';
-        
-    }
-    else
-  		echo '<p>Vérifiez vos saisies</p>';
-    /*  \[\frac{numerateur}{denominateur}\] */
-    
-    //si division a/mod contient un '.', partie entiere
-    if(preg_match('#\.#', $_POST['entierA']/$_POST['modulo'])){
-        echo '\[ \lfloor \dfrac{'.$_POST['entierA'].'}{'.$_POST['modulo'].'} \rfloor = '.partieEntiere($_GET['entierA']/$_POST['modulo']).'\]';
-    }
-    else{
+        and trim($_POST['entierA'])!='' and trim($_POST['modulo'])!=''
+        and preg_match('#^-?[0-9]+$#', $_POST['entierA']) and preg_match('#^-?[0-9]+$#', $_POST['modulo']) 
+        ) {
+      //  \[\equiv _{indice}\]
+      echo '\['.$_POST['entierA'].'\equiv_{'.$_POST['modulo'].'}'.$_POST['entierA']%$_POST['modulo'].'\]';
+
+      //si division a/mod contient un '.', partie entiere
+      if(preg_match('#\.#', $_POST['entierA']/$_POST['modulo']) ){
+        echo '\[ \lfloor \dfrac{'.$_POST['entierA'].'}{'.$_POST['modulo'].'} \rfloor = '.partieEntiere($_POST['entierA']/$_POST['modulo']).'\]';
+      }
+      else{ /*  \[\frac{numerateur}{denominateur}\] */
         echo '\[\frac{'.$_POST['entierA'].'}{'.$_POST['modulo'].'} = '.$_POST['entierA']/$_POST['modulo'].'\]';
+      }
+      echo '\['.$_POST['entierA'].' = '.partieEntiere($_POST['entierA']/$_POST['modulo']).' \times '.$_POST['modulo'].' + \boxed{'.$_POST['entierA']%$_POST['modulo'].'} \]';
     }
-    
-    echo '\['.$_POST['entierA'].' = '.partieEntiere($_POST['entierA']/$_POST['modulo']).' \times '.$_POST['modulo'].' + \boxed{'.$_POST['entierA']%$_POST['modulo'].'} \]';
-    
     
     ?>
 </body>
