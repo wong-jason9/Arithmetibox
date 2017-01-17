2e7ec3ed7d88d606e2d5617d7cef1c018263e08d
Jérémy Dos Santos <jeremydossantos96@yahoo.fr>
2017-01-17 02:16:21 +0100

    RSA Cryptage LATEX

--- Contenu/rsa.php
+++ Contenu/rsa.php
@@ -1,9 +1,9 @@
 <?php require('debut.php'); ?>
 <h2 class="titreDansLaFonctions">RSA</h2>
 <form action='Arithmetibox.php?outil=rsa' method='post'>
-<label>Décryptage<input type='radio' name='fonction' value='decrypt' checked='checked'></label>
+<label>Décryptage<input type='radio' name='fonction' value='decrypt'></label>
 <label>Cryptage<input type='radio' name='fonction' value='crypt'></label></p>
-Alphabet : <input size='50' name='alphabet' type='text' value='0123456789'><br>
+Alphabet : <input size='50' name='alphabet' type='text' value='ABCDEFGHIJKLMNOPQRSTUVWXYZ'><br>
 Clef: <input size='43' name='clef' type='text'><br>
 <p>Message :<br>
 </p>
@@ -13,61 +13,102 @@ Clef: <input size='43' name='clef' type='text'><br>
 
 <?php
 require('fonctions.php');
+
+function size($var){
+	$i=0;
+	foreach($var as $v) $i++;
+	return $i;
+}
     
 
-if(isset($_POST['alphabet']) and isset($_POST['message']) and isset($_POST['clef']) and $_POST['method']='crypt'){
+if(isset($_POST['alphabet']) and isset($_POST['message']) and isset($_POST['clef']) and $_POST['fonction']='decrypt'){ 
 	$alphabet=str_split($_POST['alphabet']);
 	$clef=explode(' ',$_POST['clef']);
 	$mess=explode(' ',$_POST['message']);
-	$messCrypter=array();
-	$i=0;
-	$primaryclef=decomposition($clef[0]);
-	$phi=($primaryclef[0]-1)*($primaryclef[1]-1);
-	if(pgcd($phi,$clef[1])==1)echo "Clef valide";
-	foreach($mess as $val){
-		$messCrypter[$i]=expoModRapide($val,$clef[1],$clef[0]);
-	}
-	foreach($messCrypter as $val){
-		echo $val;
-	}
-	if(isset($_POST['alphabet']) and isset($_POST['message']) and isset($_POST['clef']) and $_POST['method']='decrypt'){ 
-		// en supposant que l'utilisateur connaisse la clé publique sinon utilisé la clé publique pour la retrouvé
-		$alphabet=str_split($_POST['alphabet']);
-		$clef=explode(' ',$_POST['clef']);
-		$mess=explode(' ',$_POST['message']);
-		$messDecrypter=array();
-		$phi=($primaryclef[0]-1)*($primaryclef[1]-1);
-		echo "phi=".$phi;
-		if(pgcd($phi,$clef[1])==1)echo "Clef valide";
-		foreach($mess as $val){
-			$messDecrypter[$i]=expoModRapide($val,$clef[1],$clef[0]);
-		}
-		foreach($messDecrypter as $val){
-		echo "Message initial :".var_dump($mess);
-		echo "Clef publique : (".$clef[0].",".$clef[1].")";
-		var_dump($val);
-		}	
-	}
+    $mess=str_split($_POST['message']);
+    // Tester la clef
 
-	elseif(isset($_POST['alphabet']) and isset($_POST['message']) and isset($_POST['clef']) and $_POST['method']='decrypt'){ 
-		//sinon utilisé la clé publique pour la retrouvé
-		$alphabet=str_split($_POST['alphabet']);
-		$clef=explode(' ',$_POST['clef']);
-		$mess=explode(' ',$_POST['message']);
-		$messDecrypter=array();
-		$phi=($primaryclef[0]-1)*($primaryclef[1]-1);
-		$resEuclEtendu=euclEtendu($phi,$clef[1]);
-		echo "clé privée (".$clef[0].",".$resEuclEtendu[v].")\n";
-		if(pgcd($phi,$clef[1])==1)echo "Clef valide";
-		foreach($mess as $val){
-			$messCrypter[$i]=expoModRapide($val,$resEuclEtendu[$v],$clef[0]);
-		}
-		foreach($messDecrypter as $val){
-		echo $val;
-		}
-	}
+    // Transformer mess grace a l'indice du tableaux et le reconvertir a la fin
 
+    // Decomposition clef a
+    $tabPremiers=era($clef[0]);
+    $res=decomposition($clef[0]);
+    $res[1]=gmp_div($clef[0],$res[0]);
+    echo "le nombre ".$clef[0]." est compose de ".$res[0]." x ".$res[1]."<br/>";
+    // calcul de phi
+    $phi=($res[0]-1)*($res[1]-1);
+    if(($res[0]-1)<($res[1]-1) && pgcd($phi,clef[1]==1)) echo "Clef de cryptage valide";
+    echo "phi=".$phi."</br>";
+    echo "Le message crypter est : ";
+    // Expo rapide + affichage
+    echo "\$\$";
+    echo "\\begin{array}{";
+    for($compt=0;$compt<size($mess);$compt++) echo "|c";
+    echo "}";
+        for($j=0;$j<size($mess);$j++){
+            echo $mess[$j].'&';
+        for($k=0;$k<size($alphabet);$k++){
+            if($mess[$j]==$alphabet[$k]){
+                $messConv[$j]=$k;
+                $valIndice[$j]=$k;
+            }
+        }
+    }
+    echo "\\\\\\hline";
+    for($z=0;$z<size($valIndice);$z++){
+        echo $valIndice[$z]."&";
+    }
+    echo "\\\\\\hline";
+    for($i=0;$i<size($messConv);$i++){
+        $resExpoRap[$i]=expoModRapide($messConv[$i],$clef[1],$clef[0]);
+        echo $resExpoRap[$i]."&" ;
+    }
+    echo "\\\\";
+    echo"\\end{array}".'\\\\';
+    echo "\$\$";
+   /*     for($j=0;$j<size($resExpoRap);$j++){
+            for($k=0;$k<size($alphabet);$k++){
+                if($resExpoRap[$j]==$k){
+                    $messLettre[$j]=$alphabet[$k];
+            }
+        }
+    }*/ // Fonctionne quand l'expo rapide retombe sur une lettre de l'alphabet
 }
+elseif(isset($_POST['alphabet']) and isset($_POST['message']) and isset($_POST['clef']) and $_POST['fonction']='crypt'){ // PB du reduire les conditons
+        $alphabet=str_split($_POST['alphabet']);
+    $clef=explode(' ',$_POST['clef']);
+    $mess=explode(' ',$_POST['message']);
+    $mess=str_split($_POST['message']);
+    // Tester la clef
+
+    // Transformer mess grace a l'indice du tableaux et le reconvertir a la fin
+    for($j=0;$j<size($mess);$j++){
+        for($k=0;$k<size($alphabet);$k++){
+            if($mess[$j]==$alphabet[$k]){
+                $messConv[$j]=$k;
+            }
+        }
+    }
+    // Decomposition clef a
+    $tabPremiers=era($clef[0]);
+    $res=decomposition($clef[0]);
+    $res[1]=gmp_div($clef[0],$res[0]);
+    echo "le nombre ".$clef[0]." est compose de ".$res[0]." x ".$res[1]."<br/>";
+    // calcul de phi
+    $phi=($res[0]-1)*($res[1]-1);
+    // Calcul d'inverse pour clé privée
+    $invClef=gmp_gcdext($phi,$clef[1]);
+    var_dump($invClef);
+    if(($res[0]-1)<($res[1]-1) && pgcd($phi,clef[1]==1)) echo "Clef de cryptage valide";
+    echo "phi=".$phi."</br>";
+    echo "Le message decrypter est : </br>";
+    // Expo rapide
+    for($i=0;$i<size($messConv);$i++){
+        $resExpoRap[$i]=expoModRapide($messConv[$i],$invClef[$v],$clef[0]);
+        echo $resExpoRap[$i]." " ;
+    }
+}   
+
     ?>
 </body>
 </html>
