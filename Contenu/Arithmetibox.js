c0902b8eb6ec26d98d710ee63ffbefea1b8b9ba1
jrKaing <jack.kaing@gmail.com>
2017-03-24 20:23:19 +0100

    recuperer msg dans js

--- Contenu/Arithmetibox.js
+++ Contenu/Arithmetibox.js
@@ -2,11 +2,14 @@ console.log("Programme attaque vient d'être chargé");
 $(document).ready(function(){
 	console.log("Document prêt");
 
+	//Gestionnaire lorsqu'on clique sur le bouton changer
 	$('#change').click(function(){
 		console.log("appuie sur le bouton changer");
+		var message = $('.message').text();
 		$.post("Contenu/changerSubstitution.php",
 			{
 				ok: true,
+				message: message,
 			},
 			function(reponse){
 				console.log(reponse);
--- Contenu/changerSubstitution.php
+++ Contenu/changerSubstitution.php
@@ -1,6 +1,8 @@
 <?php
 
 $req = $_POST['ok'];
+$message = $_POST['message'];
+
 if($req){
 	echo "J'ai reçu votre requête";
 }
